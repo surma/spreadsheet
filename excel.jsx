8873175f4ca604cf987f3822927c803b6b0fb0e2
Surma <surma@surma.dev>
2022-01-05 01:59:13 +0000

    Properly iterate until no change

--- excel.jsx
+++ excel.jsx
@@ -1,7 +1,6 @@
 /** @jsx h */
-import { h, Fragment } from "preact";
+import { h } from "preact";
 import { useState } from "preact/hooks";
-import cleanSet from "clean-set";
 
 function range(length) {
   return Array.from({ length }).map((_, i) => i);
@@ -20,37 +19,43 @@ function excelColumn(idx) {
 }
 
 function newSpreadsheet(cols, rows) {
-  return Array.from({ length: rows }, () =>
-    Array.from({ length: cols }).map(() => ({
-      formula: "0",
-      value: 0,
-    }))
-  );
+  return {
+    cells: Array.from({ length: rows }, () =>
+      Array.from({ length: cols }).map(() => ({
+        formula: "0",
+        value: 0,
+      }))
+    ),
+  };
 }
 
 function iterate(sheet) {
-  for (let y = 0; y < sheet.length; y++) {
-    const row = sheet[y];
+  let didAnUpdate = false;
+  for (let y = 0; y < sheet.cells.length; y++) {
+    const row = sheet.cells[y];
     for (let x = 0; x < row.length; x++) {
       const cell = row[x];
-      sheet = cleanSet(
-        sheet,
-        [y, x, "value"].join("."),
-        evalFormula(sheet, cell.formula)
-      );
+      const newValue = evalFormula(sheet, cell.formula);
+      didAnUpdate = didAnUpdate || newValue != cell.value;
+      cell.value = newValue;
     }
   }
-  return sheet;
+  return didAnUpdate;
+}
+
+function update(sheet) {
+  while (iterate(sheet));
 }
 
 function evalFormula(sheet, formula) {
   const code = `
 		(function() {
-			${sheet
+			${sheet.cells
         .map((row, y) =>
           row
             .map(
-              (cell, x) => `const ${excelColumn(x)}${y} = ${sheet[y][x].value};`
+              (cell, x) =>
+                `const ${excelColumn(x)}${y} = ${sheet.cells[y][x].value};`
             )
             .join("\n")
         )
@@ -65,9 +70,12 @@ export default function ({ cols, rows }) {
   const [sheet, setSheet] = useState(newSpreadsheet(cols, rows));
 
   function setCellFormula(sheet, x, y, v) {
-    sheet = cleanSet(sheet, [y, x, "formula"].join("."), v);
-    sheet = iterate(sheet);
-    setSheet(sheet);
+    sheet.cells[y][x].formula = v;
+    update(sheet);
+    // Avoid object equality so preact doesn’t debounce.
+    setSheet({
+      cells: sheet.cells,
+    });
   }
 
   return (
@@ -87,7 +95,7 @@ export default function ({ cols, rows }) {
                 row={row}
                 col={col}
                 setCellFormula={(v) => setCellFormula(sheet, col, row, v)}
-                cell={sheet[row][col]}
+                cell={sheet.cells[row][col]}
               />
             </td>
           ))}
